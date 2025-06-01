a4e7e3d15c657ad6f48c4d8e277d5ed51a58203d
Joao Gabriel <jonhjonh22@outlook.com>
2025-05-31 21:40:44 -0300

    contantes updade

--- src/mobile/constants/Colors.ts
+++ src/mobile/constants/Colors.ts
@@ -1,6 +1,6 @@
 /**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ * Abaixo estão as cores utilizadas no aplicativo. As cores são definidas no modo claro e escuro.
+ * Existem várias outras maneiras de estilizar seu aplicativo. Por exemplo, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
 const tintColorLight = '#0a7ea4';
